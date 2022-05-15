9b06ba646866df0f645557f925fea4e5a2c12fd3
a0988948448@gmail.com <a0988948448@gmail.com>
2022-05-16 00:00:59 +0300

    automations rozetka

---
+++ page_objects/catalog/fragments/title_block.fragment.js
@@ -0,0 +1,10 @@
+class TitleBlockFragment {
+    constructor() {
+      this.root = $('.catalog-heading,h1')
+      this.title = this.root.$('h1')
+    }
+  }
+  
+  module.exports = {
+    TitleBlockFragment
+  }
